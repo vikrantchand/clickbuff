127284917b967bb4837fd7e9528b6722739cd4d6
clickbuff <dev.clickbuff@gmail.com>
2014-01-10 10:18:07 +0530

    Spring seciruty Added

--- src/main/java/com/clickbuff/dao/impl/AdDaoImpl.java
+++ src/main/java/com/clickbuff/dao/impl/AdDaoImpl.java
@@ -1,9 +1,11 @@
 package com.clickbuff.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import com.clickbuff.commons.dao.GenericJpaDao;
 import com.clickbuff.dao.AdDao;
 import com.clickbuff.model.Ad;
-
+@Repository
 public class AdDaoImpl extends GenericJpaDao<Ad, Integer> implements AdDao {
 
 	public AdDaoImpl() {
--- src/main/java/com/clickbuff/dao/impl/MessageDaoImpl.java
+++ src/main/java/com/clickbuff/dao/impl/MessageDaoImpl.java
@@ -1,9 +1,11 @@
 package com.clickbuff.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import com.clickbuff.commons.dao.GenericJpaDao;
 import com.clickbuff.dao.MessageDao;
 import com.clickbuff.model.Message;
-
+@Repository
 public class MessageDaoImpl extends GenericJpaDao<Message, Integer> implements
 		MessageDao {
 
